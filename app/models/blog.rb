2f2b9cd4c8fbb9f556772d24420321afe55ceaa7
junho jang <cclemon3248@gmail.com>
2022-09-04 06:15:02 +0900

    バリデーション、文字数の上限を設定

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,9 @@
 class Blog < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
   validates :start_date, presence: true
   validates :end_date, presence: true
   validate :end_check
-  
+  validates :memo, length: { maximum: 500 }
   def end_check
     if self.end_date == nil || self.end_date < Date.today
     errors.add(:end_date, "は今日以降の日付で選択してください")
